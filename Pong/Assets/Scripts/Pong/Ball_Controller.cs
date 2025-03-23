954522fab606e14d523fe5409e248ccb972f45bb
Craig McCorrisken <craigmccorrisken@gmail.com>
2025-03-23 15:45:53 +0000

    Updated Pong: - Added a delay to the start of the match and on reset to allow for ease of play - Added a random direction for the start to allow fairer gameplay - Updated code for ball controller for simplicity
    Signed-off-by: Craig McCorrisken <craigmccorrisken@gmail.com>

--- Pong/Assets/Scripts/Pong/Ball_Controller.cs
+++ Pong/Assets/Scripts/Pong/Ball_Controller.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class SphereController : MonoBehaviour
 {
-    float thrust;
     Rigidbody2D m_Rigidbody;
     Score_Manager score;
-
-    public void init()
-    {
-        //Fetch the Rigidbody component you attach from your GameObject
-        m_Rigidbody = GetComponent<Rigidbody2D>();
-        //Set the speed of the GameObject
-        thrust = 500.0f;
-        score = GameObject.Find("Score Manager").GetComponent<Score_Manager>();
-    }
+    float thrust;
+    const float BALL_VELOCITY = 300.0f;
 
     private void starting_direction()
     {
         float starting_direction_x = Random.Range(1, 10);  // creates a number between 1 and 10
         float starting_direction_y = Random.Range(1, 10);
 
+        // X axis
         if (starting_direction_x % 2 == 0)
         {
             m_Rigidbody.AddForce(transform.right * thrust);
@@ -31,6 +24,7 @@ public class SphereController : MonoBehaviour
             m_Rigidbody.AddForce(transform.right * -thrust);
         }
 
+        // Y axis
         if (starting_direction_y % 2 == 0)
         {
             m_Rigidbody.AddForce(transform.up * thrust);
@@ -38,22 +32,6 @@ public class SphereController : MonoBehaviour
         else
         {
             m_Rigidbody.AddForce(transform.up * -thrust);
-        }
-    }
-
-    // from the start of the game, the ball will move to the left`
-    public void Start()
-    {
-        init();
-        starting_direction();
-    }
-
-    // Update is called once per frame
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            resetBall();
         }
     }
 
@@ -73,14 +51,49 @@ public class SphereController : MonoBehaviour
         }
     }
 
+    //Wait, then give the ball a velocity & direction
+    IEnumerator delay_start()
+    {
+        yield return new WaitForSeconds(0.7f);
+
+        thrust = BALL_VELOCITY;
+        starting_direction();
+    }
+
     void resetBall()
     {
-        //reset the ball
+        //reset ball position, velocity and direction (pause ball)
         m_Rigidbody.linearVelocity = new Vector3(0.0f, 0.0f, 0.0f);
         m_Rigidbody.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        thrust = 0.0f;
 
-        thrust = 500.0f;
+        //delay the ball before starting the game
+        StartCoroutine(delay_start());
+    }
 
-        starting_direction();
+    public void init()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        //Fetch the Score Manager component
+        score = GameObject.Find("Score Manager").GetComponent<Score_Manager>();
+    }
+
+    // from the start of the game, the ball will move to the left`
+    public void Start()
+    {
+        init();
+        //delay the ball before starting the game
+        StartCoroutine(delay_start());
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            resetBall();
+        }
     }
 }
